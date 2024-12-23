0923db128d5801aff8fcd54581a30eebf1333af3
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-23 23:08:28 +0530

    redesigned the event cards

--- components/back/EventCard.jsx
+++ components/back/EventCard.jsx
@@ -16,7 +16,7 @@ function EventCard() {
   };
 
   return (
-    <div className="w-[250px] h-[350px] bg-white border-2 border-[#323232] rounded-[5px] shadow-[4px_4px_#323232] flex flex-col justify-start p-[20px] gap-[10px] hover:scale-105 duration-200">
+    <div className="w-[250px] h-[350px] bg-white rounded-[10px] flex flex-col justify-start p-[20px] gap-[10px] hover:scale-105 duration-200">
       <div className="transition-all duration-500 flex justify-center">
         {/* Card Image with fixed size */}
         <div className="w-full h-[120px] relative border-2 rounded-md border-black bg-gradient-to-t from-transparent to-[rgba(0,0,0,0.3)]">
@@ -29,35 +29,39 @@ function EventCard() {
       </div>
 
       <div className="flex flex-col items-center">
-        <h3 className="text-[20px] font-bold text-black hover:text-[#1d4ed8] transition-colors duration-300">
+        <h3 className="text-[20px] font-bold text-black mt-2 hover:text-[#1d4ed8] transition-colors duration-300">
           {event.title}
         </h3>
-        <p className="text-[14px] max-w-[200px] font-normal text-[#4a4a4a] truncate">
+        {/* <p className="text-[14px] max-w-[200px] font-normal text-[#4a4a4a] mt-1 truncate">
           {event.description}
-        </p>
+        </p> */}
 
-        <div className="mt-[10px] text-[14px] text-[#323232] flex flex-col items-center gap-[5px]">
+        <div className="mt-[10px] text-[14px] text-[#323232] flex flex-col items-start gap-[5px]">
           <p className="text-sm">
-            <strong className="text-[#000000]">Date:</strong>{" "}
+            <strong className="font-bold text-[#000000]">Date:</strong>{" "}
             <span className="text-black">
               {new Date(event.date).toLocaleDateString()}
             </span>
           </p>
           <p className="text-sm">
-            <strong className="text-[#000000]">Registration Fee:</strong>{" "}
+            <strong className="font-bold text-[#000000]">
+              Registration Fee:
+            </strong>{" "}
             <span className="text-black">
               {event.registrationFee ? `₹${event.registrationFee}` : "Free"}
             </span>
           </p>
           <p className="text-sm">
-            <strong className="text-[#000000]">Slots Remaining:</strong>{" "}
+            <strong className="font-bold text-[#000000]">
+              Slots Remaining:
+            </strong>{" "}
             <span className="text-black">{event.capacity} slots</span>
           </p>
         </div>
       </div>
 
       {/* Footer with Action Buttons */}
-      <div className="flex gap-2 justify-between">
+      <div className="flex gap-2 justify-between mt-3">
         <a
           to={`/events/${event._id}`}
           className="text-xs py-[8px] px-[10px] w-full bg-black text-white font-medium text-center rounded-[5px] hover:bg-white hover:text-black border-2 border-black transition-colors duration-300"
