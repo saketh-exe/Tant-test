34c325f7d307e76eca5f17661d91f6a722004a28
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-23 22:39:41 +0530

    modified margin and alignment for about and featured sections

--- components/back/Featured.jsx
+++ components/back/Featured.jsx
@@ -4,7 +4,7 @@ export default function Featured() {
   return (
     <div className="font-semibold text-center text-white text-5xl sm:text-5xl md:text-5xl lg:text-5xl w-11/12 sm:w-3/4 mx-auto my-10 mb-34 bg-slate-50 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-lg p-6">
       Featured
-      <div className="flex flex-col sm:flex-row justify-evenly align-middle gap-4 m-6 sm:m-12 md:m-24">
+      <div className="flex flex-col sm:flex-row justify-evenly align-middle gap-4 m-6 sm:m-12 md:m-24zz">
         <EventCard />
         <EventCard />
         <EventCard />
