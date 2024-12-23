6f0d95dbe196e33842767fcc39986f02c2b26b8d
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-23 22:16:37 +0530

    typo errors corrected

--- components/back/About.jsx
+++ components/back/About.jsx
@@ -9,21 +9,21 @@ export default function About() {
         Vidyapeetham, Chennai Campus. The festival celebrates the spirit of
         innovation, engineering, and technology with a diverse range of
         competitions, workshops, lectures, and exhibitions. The name
-        "Tantrotrotsav" combines the Sanskrit words "Tantra," meaning
-        technology, and "Utsav," meaning festival, symbolizing a vibrant
-        celebration of technical prowess and creativity. This fest features an
-        array of events such as hackathons, coding challenges, robotics
-        competitions, tech-talks by eminent personalities, panel discussions,
-        and hands-on workshops aimed at fostering technical skills and
-        knowledge. Exhibitions showcasing cutting-edge projects and
-        demonstrations of emerging technologies are also key highlights of the
-        event. Traditionally held over three action-packed days, Tantrotrotsav
-        is entirely organized and managed by the students of Amrita Chennai
-        Campus, emphasizing teamwork, leadership, and innovation. It attracts
-        participants from all over India, making it a hub for collaboration,
-        networking, and the exchange of ideas. Having grown significantly since
-        its inception, Tantrotrotsav continues to inspire and empower young
-        minds to think beyond boundaries and shape the future of technology.{" "}
+        "Tantrotsav" combines the Sanskrit words "Tantra," meaning technology,
+        and "Utsav," meaning festival, symbolizing a vibrant celebration of
+        technical prowess and creativity. This fest features an array of events
+        such as hackathons, coding challenges, robotics competitions, tech-talks
+        by eminent personalities, panel discussions, and hands-on workshops
+        aimed at fostering technical skills and knowledge. Exhibitions
+        showcasing cutting-edge projects and demonstrations of emerging
+        technologies are also key highlights of the event. Traditionally held
+        over three action-packed days, Tantrotsav is entirely organized and
+        managed by the students of Amrita Chennai Campus, emphasizing teamwork,
+        leadership, and innovation. It attracts participants from all over
+        India, making it a hub for collaboration, networking, and the exchange
+        of ideas. Having grown significantly since its inception, Tantrotsav
+        continues to inspire and empower young minds to think beyond boundaries
+        and shape the future of technology.{" "}
       </div>
     </div>
   );
