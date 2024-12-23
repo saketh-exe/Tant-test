803617559a8d4c0745d35f90e291ce9a8d08eec0
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-23 09:20:02 +0530

    changes

--- components/ui/Herotext.jsx
+++ components/ui/Herotext.jsx
@@ -6,12 +6,11 @@ export default function Herotext() {
     ["T", "A", "N", "T", "R", "O", "T", "S", "A", "V"],
     ["तं", "त्रो", "त्स", "व"],
     ["తం", "త్రో", "త్స", "వం"],
-    ["த", "ந்", "தி", "ரோ", "த்", "ச", "வ்" ],
-    ["ത", "ന്ത്രോ", "ത്സ", "വം" ],
-    ["탄", "트", "로", "츠", "바" ],
-    ["タ", "ン", "ト", "ロ", "ツ", "バ" ],
-    ["坦", "特", "罗", "茨", "瓦" ],
-   
+    ["த", "ந்", "தி", "ரோ", "த்", "ச", "வ்"],
+    ["ത", "ന്ത്രോ", "ത്സ", "വം"],
+    ["탄", "트", "로", "츠", "바"],
+    ["タ", "ン", "ト", "ロ", "ツ", "バ"],
+    ["坦", "特", "罗", "茨", "瓦"],
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -38,7 +37,6 @@ export default function Herotext() {
         // zIndex: 2,
         // position: "fixed",
         width: "100%",
-        
         textAlign: "center",
         fontSize: "8rem",
         fontWeight: "900",
@@ -49,29 +47,34 @@ export default function Herotext() {
         flexDirection: "column",
       }}
     >
-      <span className="font-medium text-6xl mb-4 text-yellow-100">Welcome to </span>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={currentIndex}
-          style={{ display: "flex", justifyContent: "center", gap: "1px" }}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
-        >
-          {[...text[currentIndex]].map((letter, index) => (
-            <motion.span
-              key={`${currentIndex}-${index}`}
-              variants={textVariants}
-              transition={{ duration: 1, type: "spring" }}
-              style={{textShadow: "0 0 20px wheat",color: "white"}}
-              className="herotxt"
-            >
-              {letter+(index === text[currentIndex].length - 1 ? " '25" : "")}
-            </motion.span>
-          ))}
-        </motion.div>
-      </AnimatePresence>
+      <span className="font-medium text-6xl mb-4 text-yellow-100">
+        Welcome to{" "}
+      </span>
+      <div className="relative top-10">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentIndex}
+            style={{ display: "flex", justifyContent: "center", gap: "1px" }}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={{ visible: { transition: { staggerChildren: 0.1 } } }}
+          >
+            {[...text[currentIndex]].map((letter, index) => (
+              <motion.span
+                key={`${currentIndex}-${index}`}
+                variants={textVariants}
+                transition={{ duration: 1, type: "spring" }}
+                style={{ textShadow: "0 0 20px wheat", color: "white" }}
+                className="herotxt"
+              >
+                {letter +
+                  (index === text[currentIndex].length - 1 ? " '25" : "")}
+              </motion.span>
+            ))}
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </div>
   );
 }
