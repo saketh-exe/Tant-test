0ce496d69926227a12c737db9f0d4903d5a90886
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-24 10:31:46 +0530

    adjusted alignments in countdown responsiveness

--- components/back/Coundown.jsx
+++ components/back/Coundown.jsx
@@ -36,33 +36,39 @@ export default function Coundown() {
   });
 
   return (
-    <div className="flex justify-evenly w-3/4 text-6xl align-middle h-fit mt-24 text-slate-300">
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center w-1/6 rounded-lg">
+    <div className="flex justify-evenly w-full gap-4 lg:text-4xl text-2xl align-middle h-fit mt-24 text-slate-300">
+      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center lg:w-1/6 w-1/4 rounded-lg">
         {" "}
         {/* Days */}
-        <span className="text-center mt-4 ">{timeLeft.days}</span>
-        <span className="text-pink-300 text-2xl mt-4">Days</span>
+        <span className="text-center mt-4">{timeLeft.days}</span>
+        <span className="text-pink-300 text-lg lg:text-2xl sm:text-lg mt-4">
+          Days
+        </span>
       </div>
 
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center w-1/6 rounded-lg">
+      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center lg:w-1/6 w-1/4 rounded-lg">
         {" "}
         {/* Hours */}
         <span className="text-center mt-4">{timeLeft.hours}</span>
-        <span className="text-pink-300 text-2xl mt-4">Hours</span>
+        <span className="text-pink-300 text-lg lg:text-2xl sm:text-lg mt-4">
+          Hours
+        </span>
       </div>
 
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center w-1/6 rounded-lg">
+      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center lg:w-1/6 w-1/4 rounded-lg">
         {" "}
         {/* Minutes */}
         <span className="text-center mt-4">{timeLeft.minutes}</span>
-        <span className="text-pink-300 text-2xl mt-4">Minutes</span>
+        <span className="text-pink-300 text-lg lg:text-2xl sm:text-lg mt-4">
+          Minutes
+        </span>
       </div>
 
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center w-1/6 rounded-lg">
+      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-8 flex flex-col justify-center items-center lg:w-1/6 w-1/4 rounded-lg">
         {" "}
         {/* Seconds */}
         <span className="text-center mt-4">{timeLeft.seconds}</span>
-        <span className="text-pink-300 text-2xl mt-4 font-semibold">
+        <span className="text-pink-300 text-lg lg:text-2xl sm:text-lg mt-4">
           Seconds
         </span>
       </div>
