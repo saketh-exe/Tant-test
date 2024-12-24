0c53dc5605f938b43b581e6c6eca99126bed4f5d
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-24 11:29:07 +0530

    designed maps card in contact page

--- components/back/PinDemo.jsx
+++ components/back/PinDemo.jsx
@@ -3,13 +3,13 @@ import { PinContainer } from "../ui/Pin";
 import im from "./image.png";
 export default function PinDemo() {
   return (
-    <div className="h-auto w-full flex items-center justify-center ">
+    <div className="h-auto w-full flex items-center justify-center">
       <PinContainer
-        title="/ui.aceternity.com"
+        title="Amrita Chennai"
         href="https://maps.app.goo.gl/KSXseiWPrpSjqXRVA"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-black">
             Amrita Vishwa Vidyapeetham
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
